49b574b482c1ee7972093e45e872209a18d0b479
k1rurk <leonov.k.e@edu.mirea.ru>
2024-05-14 17:43:44 +0300

    fix: fix dns that stops the app

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"dependency-confusion/internal/api"
-	log "github.com/sirupsen/logrus"
 	"dependency-confusion/internal/database"
-	"dependency-confusion/internal/dns_exfiltrate"
 	"dependency-confusion/runconfig"
+
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 // @title           Swagger Example API
@@ -24,7 +24,6 @@ import (
 // @host      localhost:9000
 // @BasePath  /api/v1
 
-
 // @externalDocs.description  OpenAPI
 // @externalDocs.url          https://swagger.io/resources/open-api/
 func main() {
@@ -39,7 +38,7 @@ func main() {
 	}
 
 	// start DNS
-	dns_exfiltrate.RunDNS(sql, &cfg.DNSConfig)
+	//go dns_exfiltrate.RunDNS(sql, &cfg.DNSConfig)
 
 	//gin.SetMode(gin.ReleaseMode)
 	gin.SetMode(gin.DebugMode)
